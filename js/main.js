f10a758451f7d8ea2f49b73a0dd43b7dc5601641
weborastudio1 <weborastudiozz@gmail.com>
2026-04-27 14:52:10 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -266,9 +266,9 @@ window.addEventListener('load', function() {
 
 
 const heroImages = [
-  "images/hero-1.jpg",
-  "images/hero-2.jpg",
-  "images/hero-3.jpg"
+  "images/hero-home.jpg",
+  "images/hero-home1.jpg",
+  "images/hero-home2.jpg"
 ];
 
 let currentIndex = 0;
